99e20d1ea4cd8f722686ad469bfa09d55d3f16e9
Omar Tarek <omartarek711@gmail.com>
2023-03-19 00:36:56 +0200

    Add sort and select filters

--- controllers/products.js
+++ controllers/products.js
@@ -9,14 +9,29 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-	const { featured, company, name} = req.query;
+	const { featured, company, name, sort, fields} = req.query;
 	const queryObject = {};
 
 	if (featured) queryObject.featured = featured === "true" ? true : false;
 	if (company) queryObject.company = company;
-  if (name) queryObject.name = { $regex: name, $options: "i" };
+	if (name) queryObject.name = { $regex: name, $options: "i" };
 
-	const products = await Product.find(queryObject).sort("name");
+	const result = Product.find(queryObject);
+  
+	if (sort) {
+		const sortList = sort.split(",").join(" ");
+    result.sort(sortList);
+	} else {
+    result.sort("createdAt name");
+  }
+
+  if(fields) {
+    const fieldsList = fields.split(',').join(' ');
+    result.select(fieldsList);
+  }
+
+	const products = await result;
+
 	res.status(200).json({ products, nbHits: products.length });
 };
 
