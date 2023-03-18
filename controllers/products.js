fd41a4ec4898d3337b60a074097fb17a61bf5e70
Omar Tarek <omartarek711@gmail.com>
2023-03-19 00:52:39 +0200

    Add pagination to all products

--- controllers/products.js
+++ controllers/products.js
@@ -17,7 +17,7 @@ const getAllProducts = async (req, res) => {
 	if (name) queryObject.name = { $regex: name, $options: "i" };
 
 	const result = Product.find(queryObject);
-  
+
 	if (sort) {
 		const sortList = sort.split(",").join(" ");
     result.sort(sortList);
@@ -29,6 +29,12 @@ const getAllProducts = async (req, res) => {
     const fieldsList = fields.split(',').join(' ');
     result.select(fieldsList);
   }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result.skip(skip).limit(limit);
 
 	const products = await result;
 
